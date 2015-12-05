a35a5bd549159b24eaf37bdcc45e9506ebd1c03d
Alex Zywiak <alex.zywiak@gmail.com>
2015-12-05 14:37:04 -0500

    Working JSON Transcript with timestamps

---
+++ transcript.js
@@ -0,0 +1,30 @@
+'use strict';
+
+var watson = require('watson-developer-cloud');
+var fs = require('fs');
+var config = require('./config');
+
+var speech_to_text = watson.speech_to_text({
+  username: config.username,
+  password: config.password,
+  version: 'v1',
+  url: 'https://stream.watsonplatform.net/speech-to-text/api',
+});
+
+var params = {
+  audio: fs.createReadStream('./0001.flac'),
+  content_type: 'audio/flac',
+  timestamps: true
+};
+
+speech_to_text.recognize(params, function(err, transcript) {
+  if (err)
+    console.log(err);
+  else
+    console.log(JSON.stringify(transcript, null, 2));
+  	fs.writeFile('transcript.json', JSON.stringify(transcript, null, 2), function(err){
+  		if(err) console.log(err);
+  		else 
+  			console.log(transcript);
+  	})
+});
